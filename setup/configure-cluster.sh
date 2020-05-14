255213f7a835643cfe1eb09a14b3c8c9efd0c665
Shubham <ssakhuja@aramse.io>
2020-05-14 16:38:36 -0400

    add view permission for all users

--- setup/configure-cluster.sh
+++ setup/configure-cluster.sh
@@ -6,5 +6,6 @@ for user in $USERS; do
   base=$(echo $user | cut -d '@' -f 1)
   kubectl create ns $base
   kubectl create rolebinding $base -n $base --clusterrole edit --user "$user"
+  kubectl create clusterrolebinding $base-view --clusterrole view --user "$user"
 done
   
